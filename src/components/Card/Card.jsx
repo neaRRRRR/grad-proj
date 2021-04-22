5c5f91a486c1f81f311bd88c868c5bce080ee0da
neaRRRRR <kaancakici510@gmail.com>
2021-04-22 15:53:47 +0300

    Card design

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
 import './Card.styles.scss'
+import usericon from '../../assets/usericon.png'
 
 const Card = () => {
 
     return(
         <div className='card'>
             <div className='inner-card'>
-                <div className='up'>Date of design</div>
+                <div className='up'>
+                    <div className='card-date'>22.04.2021</div>
+                </div>
                 
-                <div className='middle'>Design Name</div>
+                <div className='middle' style={{borderTop:"4px solid blue"}}>
+                    <div>Design Name</div>
+                    <div style={{fontSize:"smaller",color:"#51BCDA"}}>Design Tags</div>
+                </div>
                 
-                <div className='down'>Designer's name surname</div>
+                <div className='down'>
+                    <div>
+                        <img src={usericon} className='card-image'></img>
+                    </div>
+                    <div>Designer's name surname</div>
+                </div>
             </div>
         </div>
     )
